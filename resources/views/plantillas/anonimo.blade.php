8c9edf5cfb59392d9497f6c50af6cb2dd70b64ba
Climaco05 <rxnovachrono@gmail.com>
2023-03-13 00:54:28 -0600

    Actualizaciòn del proyecto 13-Marzo

--- resources/views/plantillas/anonimo.blade.php
+++ resources/views/plantillas/anonimo.blade.php
@@ -9,8 +9,120 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="{{ asset('estilos/styleanonimo.css') }}">
     </head>
 <body>
-ANONIMO
+
+<header>
+<div class="logo">
+<img src="../img/valo2.png" alt="logo de la empresa">
+<h2 class="nombre_empresa">SALON DE EVENTOS VALORANT</h2>
+</div>
+
+<nav>
+
+<a href="" class="nav-link">Inicio</a>
+<a href="" class="nav-link">Acerca de nosotros</a>
+<a href="" class="nav-link">Contactanos</a>
+
+</nav>
+
+</header>
+
+
+
+<body2>
+<div class="card">
+    <div class="face front">
+        <img src="../img/bautizo.jpg" alt="">
+        <h3>Bautizo</h3>
+    </div>
+
+    <div class="face back">
+        <h3>Bautizo</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas libero, odit aperiam repellendus blanditiis saepe ad? Veritatis tempore velit accusamus modi, facere nostrum mollitia facilis! At perferendis corporis quaerat deserunt?</p>
+        <div class="link">
+            <a href="#">Detalles</a>
+        </div>
+        </div>
+</div>
+
+<div class="card">
+    <div class="face front">
+        <img src="../img/boda.jpeg" alt="">
+        <h3>Boda</h3>
+    </div>
+
+    <div class="face back">
+        <h3>Boda</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas libero, odit aperiam repellendus blanditiis saepe ad? Veritatis tempore velit accusamus modi, facere nostrum mollitia facilis! At perferendis corporis quaerat deserunt?</p>
+        <div class="link">
+            <a href="#">Detalles</a>
+        </div>
+        </div>
+</div>
+
+
+<div class="card">
+    <div class="face front">
+        <img src="../img/cumpleaños.jpg" alt="">
+        <h3>Cumpleaños</h3>
+    </div>
+
+    <div class="face back">
+        <h3>Cumpleaños</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas libero, odit aperiam repellendus blanditiis saepe ad? Veritatis tempore velit accusamus modi, facere nostrum mollitia facilis! At perferendis corporis quaerat deserunt?</p>
+        <div class="link">
+            <a href="#">Detalles</a>
+        </div>
+        </div>
+</div>
+
+<div class="card">
+    <div class="face front">
+        <img src="../img/xvaños.jpg" alt="">
+        <h3>XV Años</h3>
+    </div>
+
+    <div class="face back">
+        <h3>XV Años</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas libero, odit aperiam repellendus blanditiis saepe ad? Veritatis tempore velit accusamus modi, facere nostrum mollitia facilis! At perferendis corporis quaerat deserunt?</p>
+        <div class="link">
+            <a href="#">Detalles</a>
+        </div>
+        </div>
+</div>
+
+<div class="card">
+    <div class="face front">
+        <img src="../img/convivio.jpg" alt="">
+        <h3>Reunión con familiares o amigos</h3>
+    </div>
+
+    <div class="face back">
+        <h3>Reunión con familiares o amigos</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas libero, odit aperiam repellendus blanditiis saepe ad? Veritatis tempore velit accusamus modi, facere nostrum mollitia facilis! At perferendis corporis quaerat deserunt?</p>
+        <div class="link">
+            <a href="#">Detalles</a>
+        </div>
+        </div>
+</div>
+
+<div class="card">
+    <div class="face front">
+        <img src="../img/graduacion.jpg" alt="">
+        <h3>Graduaciones</h3>
+    </div>
+
+    <div class="face back">
+        <h3>Graduaciones</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas libero, odit aperiam repellendus blanditiis saepe ad? Veritatis tempore velit accusamus modi, facere nostrum mollitia facilis! At perferendis corporis quaerat deserunt?</p>
+        <div class="link">
+            <a href="#">Detalles</a>
+        </div>
+        </div>
+</div>
+
+</body2>
 </body>
 </html>
